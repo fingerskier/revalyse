86ead6ca95d57479b8dcb7a4d68c0f7585505917
Matt Currier <fingerskier@gmail.com>
2015-12-14 14:08:05 -0600

    Initial Commit
    Basic implementation...fail...

---
+++ model/stuff.js
@@ -0,0 +1,39 @@
+// Publisher is instantiated in server/server.js
+
+Stuff.allow({
+	insert: function (userId, doc) {
+		//...
+	},
+	update: function (userId, doc, fields, modifier) {
+		//...
+	},
+	remove: function (userId, doc) {
+		//...
+	},
+	fetch: ['owner'],
+	transform: function () {
+		//...
+	}
+});
+
+Stuff.deny({
+	insert: function (userId, doc) {
+		//...
+	},
+	update: function (userId, doc, fields, modifier) {
+		//...
+	},
+	remove: function (userId, doc) {
+		//...
+	},
+	fetch: ['locked'],
+	transform: function () {
+		//...
+	}
+});
+
+Meteor.methods({
+	addStuff: function (stuffObj) {
+		// ...
+	}
+});
